90fa588922b18ee6e351d0a330faf520b89001bd
Nidhalm1 <nidhal.moussa@etu.u-paris.fr>
2026-08-12 19:48:52 +0200

    add a method to verify requests before transferring them

--- services/gateway.go
+++ services/gateway.go
@@ -1,1 +1,46 @@
 package services
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// should the server keep a list of order no i think because the order do
+type Item struct {
+	SKU string `json:"sku"`
+	Qty int    `json:"qty"`
+}
+
+type OrderRequest struct {
+	CustomerID int    `json:"customer_id"`
+	Items      []Item `json:"items"`
+}
+
+func main() {
+	// entry point
+	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/orders", createOrder)
+}
+
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	// code that handles POST /orders
+	// repond  with the status of the request after
+	ctx := r.Context()
+	// verify request is good
+	// send the htpp to the good resever and wait for the repons
+	//send the ctx with it
+	// then thenrecipient  verifiy that trancpert ? headert ?
+	var order OrderRequest
+	err := json.NewDecoder(r.Body).Decode(&order)
+	if err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+	verfiy(order)
+}
+
+// i want to add many traces sah
+func verfiy(order OrderRequest) bool {
+
+	return true
+}
